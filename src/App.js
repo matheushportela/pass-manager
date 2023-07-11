620a669fddc76faa99458384dbe54fdd36edb203
matheushportela <matheus-portela34@hotmail.com>
2023-07-11 20:45:10 -0300

    commitmsg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [siteName, setSiteName] = useState("");
+    const [passwordList, setPasswordList] = useState([]);
+
+    useEffect(() => {
+        Axios.get("http://localhost:1337/passwords").then((response) => {
+            setPasswordList(response.data);
+        });
+    }, []);
+
+    const addPassword = () => {
+        Axios.post("http://localhost:1337/add", {
+            login: login,
+            password: password,
+            siteName: siteName,
+        });
+    };
+
+    const decryptPassword = (encryption) => {
+        Axios.post("http://localhost:1337/decryptpassword", {
+            password: encryption.password,
+            iv: encryption.iv,
+        }).then((response) => {
+            navigator.clipboard.writeText(response.data);
+        });
+    };
+
+    return (
+        <div className="App">
+            <div className="AddingPassword">
+                <h1> Add Password </h1>
+                <input
+                    type="text"
+                    placeholder="email or login"
+                    onChange={(event) => {
+                        setLogin(event.target.value);
+                    }}
+                />
+                <input
+                    type="text"
+                    placeholder="password123"
+                    onChange={(event) => {
+                        setPassword(event.target.value);
+                    }}
+                />
+                <input
+                    type="text"
+                    placeholder="Google"
+                    onChange={(event) => {
+                        setSiteName(event.target.value);
+                    }}
+                />
+                <button onClick={addPassword}>Add Password</button>
+            </div>
+            <div className="Passwords">
+                {passwordList.map((val) => {
+                    return (
+                        <div
+                            className="password"
+                            key="{ key }"
+                            onClick={() => {
+                                decryptPassword({
+                                    password: val.password,
+                                    iv: val.iv,
+                                });
+                            }}
+                        >
+                            <h3>{val.siteName}</h3>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
